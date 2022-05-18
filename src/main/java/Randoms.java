583e6b771ab1c4c36b48738ace98d60e2b96c1a8
Вероника Савчук <Veronika-zhigulya@yandex.ua>
2022-05-18 10:05:41 +0300

    com

--- src/main/java/Randoms.java
+++ src/main/java/Randoms.java
@@ -1,15 +1,15 @@
 import java.util.Iterator;
-import java.util.PrimitiveIterator;
 import java.util.Random;
 
 public class Randoms implements Iterable<Integer> {
-    protected PrimitiveIterator.OfInt randomIterator;
-    protected int min, max;
+    protected Random random;
+    protected int min, max, diff;
 
     public Randoms(int min, int max) {
-        randomIterator = new Random().ints(min, (max + 1)).iterator();
         this.min = min;
         this.max = max;
+        this.diff = max - min;
+        random = new Random();
     }
 
     @Override
@@ -22,7 +22,7 @@ public class Randoms implements Iterable<Integer> {
 
             @Override
             public Integer next() {
-                return randomIterator.nextInt();
+                return random.nextInt(diff + 1) + min;
             }
         };
     }
